63aa104f2be97b581a4332ecb71ee0321fb8f5cc
Sarah Hilton <skhilton@uw.edu>
2019-07-21 20:15:58 -0700

    ngl protein on homepage, selection doesn't work

---
+++ docs/_javascript/README.md
@@ -0,0 +1,1 @@
+got ngl.js from https://unpkg.com/ngl@0.10.4/dist/ngl.js
--- docs/_javascript/prot_struct.js
+++ docs/_javascript/prot_struct.js
@@ -1,68 +1,22 @@
-// Define a global variable for protein structure application.
-var icn3dui;
+document.addEventListener("DOMContentLoaded", function () {
+  var stage = new NGL.Stage("viewport");
+  stage.setParameters({
+  backgroundColor: "white"
+})
 
-$(document).ready(function() {
-    var options = {};
-    options['proteins'] = 'sphere';
-    options['color'] = 'grey';
-    options['pk'] = 'residue';
-    options['background'] = 'white';
+// Handle window resizing
+window.addEventListener( "resize", function( event ){
+    stage.handleResize();
+}, false );
 
-    var cfg = {
-      divid: 'icn3dwrap',
-      width: 375,
-      height: 468,
-      resize: false,
-      rotate: 'none',
-      url:"?type=pdb&url=https%3A%2F%2Fwww.rcsb.org%2Fstructure%2F4O5N",
-      //pdbid: '4O5N',
-      showmenu:false,
-      showcommand: false
-    };
-    cfg
+  stage.loadFile("rcsb://4O5N").then(function (o) {
+    o.setPosition([20, 0, 0])
+    o.setRotation([ 2, 0, 0 ])
+  o.addRepresentation("spacefill", {color: "#999999"}, true)
+  var selectedAtom = o.addRepresentation("spacefill", {color: "#9900FF"}, false)
 
-    if (Object.keys(options).length > 0) cfg['options'] = options;
-    icn3dui = new iCn3DUI(cfg);
+  o.autoView()
 
-    $.when(
-      icn3dui.show3DStructure(),
-      icn3dui.downloadUrl("_data/4O5N_trimer.pdb", "pdb")
-    ).then(function() {
-      icn3dui.selectByCommand(".A,B");
-      icn3dui.showSelection();
-      icn3dui.rotStruc("right");
-      icn3dui.rotStruc("down");
-
-      document.getElementById("remove_selections").addEventListener("click", function() {
-        icn3dui.setOption('color', "grey");
-      });
-
-      // the below button allows a png export of the protein structure
-      document.getElementById("save_image").addEventListener("click", function() {
-        icn3dui.saveFile("test.png", 'png');
-      });
-      document.getElementById("send_state").addEventListener("click", function() {
-        icn3dui.shareLink();
-      });
-      document.getElementById("confirm").addEventListener("click", function() {
-        cfg['showcommand'] = true;
-        cfg['options'] = options
-        icn3dui = new iCn3DUI(cfg);
-        icn3dui.show3DStructure();
-        icn3dui.downloadUrl("_data/4O5N_trimer.pdb", "pdb");
-        icn3dui.selectByCommand(".A,B");
-        icn3dui.showSelection();
-        //icn3dui.saveFile("test_file.txt", "text");
-      });
-
-      // Select the site with the maximum y value by default.
-      var max_y_value = d3.max(chart.data, d => +d.abs_diffsel);
-      var max_y_record = chart.data.filter(d => +d.abs_diffsel == max_y_value);
-
-      if (max_y_record.length > 0) {
-        console.log("click site " + max_y_record[0].site);
-        d3.select("#site_" + max_y_record[0].site).dispatch("click");
-      }
-    });
-
+  selectedAtom.setSelection(":A and 100")
+})
 });
