b0de0434fd197fec30ec527b4f97ce554afb55f9
Sarah Hilton <skhilton@uw.edu>
2019-10-09 11:13:31 -0700

    fixed some reversions

--- docs/_javascript/line_plot_zoom.js
+++ docs/_javascript/line_plot_zoom.js
@@ -137,8 +137,7 @@ function genomeLineChart() {
 
       // Update the context brush, focus brush and zoom brush.
       brushContext.on("brush end", brushed);
-      brushFocus.on("brush end", _.debounce(brushedFocus, 200));
-      // brushFocus.on("brush end", brushedFocus);
+      brushFocus.on("brush end", brushPointsFocus);
       zoomContext.on("zoom", zoomed);
 
       //Create context plot. Shows how genome view below the focus plot
@@ -256,7 +255,7 @@ function genomeLineChart() {
         .attr("cy", YFocus)
         .attr("id", d => "site_" + d.site)
         .attr("class", "non_brushed")
-        // .style("clip-path", "url(#clip)")
+        .style("clip-path", "url(#clip)")
         .on("mouseover", showTooltip)
         .on("mouseout", hideTooltip)
         .on("click", clickOnPoint);
