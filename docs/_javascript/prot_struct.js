b4850d9f6e277e0492220faca0d92b26adc984cf
skhilton <skhilton@uw.edu>
2019-10-30 17:04:33 -0700

    protein tool tip now uses condition data

--- docs/_javascript/prot_struct.js
+++ docs/_javascript/prot_struct.js
@@ -9,12 +9,12 @@ stage.setParameters({
 });
 
 // Handle window resizing
-window.addEventListener( "resize", function( event ){
-    stage.handleResize();
-}, false );
+window.addEventListener("resize", function(event) {
+  stage.handleResize();
+}, false);
 
 // add button to the screen
-function addElement (el) {
+function addElement(el) {
   Object.assign(el.style, {
     position: "absolute",
     zIndex: 10
@@ -23,7 +23,7 @@ function addElement (el) {
 }
 
 // make button
-function createElement (name, properties, style) {
+function createElement(name, properties, style) {
   var el = document.createElement(name)
   Object.assign(el, properties)
   Object.assign(el.style, style)
@@ -31,22 +31,23 @@ function createElement (name, properties, style) {
 }
 
 // button that selects certain parts of the protein
-function createSelect (options, properties, style) {
+function createSelect(options, properties, style) {
   var select = createElement("select", properties, style)
-  options.forEach(function (d) {
+  options.forEach(function(d) {
     select.add(createElement("option", {
-      value: d[ 0 ], text: d[ 1 ]
+      value: d[0],
+      text: d[1]
     }))
   })
   return select
 }
 
 // main function
-function loadStructure (input) {
+function loadStructure(input) {
   stage.removeAllComponents()
-  return stage.loadFile(input).then(function (o) {
+  return stage.loadFile(input).then(function(o) {
     protein = o;
-    protein.setRotation([ 2, 0, 0 ])
+    protein.setRotation([2, 0, 0])
     protein.autoView()
     protein.addRepresentation(polymerSelect.value, {
       sele: "polymer",
@@ -58,48 +59,52 @@ function loadStructure (input) {
 }
 
 // color a site by a certain color
-function selectSiteOnProtein (siteString, color) {
+function selectSiteOnProtein(siteString, color) {
   // highlighted site representation should match main representation except
   // the highlighted site should be spacefill if main protein cartoon
   if (polymerSelect.value == "cartoon") {
-   fill = "spacefill";
- }else{
-   fill = polymerSelect.value
- }
- // color the site
+    fill = "spacefill";
+  } else {
+    fill = polymerSelect.value
+  }
+  // color the site
   protein.addRepresentation(fill, {
     color: color,
     name: siteString
   }).setSelection(siteString)
-  }
+}
 
 // remove color from a site
-function deselectSiteOnProteinStructure (siteString) {
+function deselectSiteOnProteinStructure(siteString) {
   stage.getRepresentationsByName(siteString).dispose()
-  }
+}
 
 // select protein display type
 var polymerSelect = createSelect([
-  [ "cartoon", "cartoon" ],
-  [ "spacefill", "spacefill" ],
-  [ "surface", "surface" ]
+  ["cartoon", "cartoon"],
+  ["spacefill", "spacefill"],
+  ["surface", "surface"]
 ], {
-  onchange: function (e) {
+  onchange: function(e) {
     stage.getRepresentationsByName("polymer").dispose()
-    stage.eachComponent(function (o) {
+    stage.eachComponent(function(o) {
       o.addRepresentation(e.target.value, {
-        sele: "polymer",
-        name: "polymer",
-        color: greyColor
-      })
-      // on change, reselect the points so they are "on top"
+          sele: "polymer",
+          name: "polymer",
+          color: greyColor
+        })
+        // on change, reselect the points so they are "on top"
       d3.selectAll(".selected").data().forEach(function(element) {
-        selectSiteOnProtein(":"+element.chain+ " and "+ element.chain_site,
-                   color_key[Math.ceil(element.abs_diffsel)])
+        selectSiteOnProtein(":" + element.chain + " and " +
+          element.chain_site,
+          color_key[Math.ceil(element.abs_diffsel)])
       });
     })
   }
-}, { top: "36px", left: "12px" })
+}, {
+  top: "36px",
+  left: "12px"
+})
 
 // tooltip setup
 tooltip = createElement("div", {}, {
@@ -107,7 +112,7 @@ tooltip = createElement("div", {}, {
   position: "absolute",
   zIndex: 10,
   pointerEvents: "none",
-  backgroundColor: "rgba(255,255,255, 0.8)",  // white and transparent
+  backgroundColor: "rgba(255,255,255, 0.8)", // white and transparent
   color: "black",
   padding: "8px",
   fontFamily: "sans-serif"
@@ -118,34 +123,39 @@ stage.viewer.container.appendChild(tooltip);
 // remove default hoverPick mouse action
 // this appears to remve the default tooltip behavior
 stage.mouseControls.remove("hoverPick")
-nan_data = {"site": NaN, "label_site": NaN, "wildtype": NaN}
+nan_data = {
+  "site": NaN,
+  "label_site": NaN,
+  "wildtype": NaN
+}
 
 // listen to `hovered` signal to move tooltip around and change its text
-stage.signals.hovered.add(function (pickingProxy) {
-  if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)){
+stage.signals.hovered.add(function(pickingProxy) {
+  if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)) {
     var atom = pickingProxy.atom || pickingProxy.closestBondAtom;
     var cp = pickingProxy.canvasPosition;
     // extract the protein chain and site
     var site_name = atom.qualifiedName().split(":")[0].split("]")[1]
     var chain_name = atom.qualifiedName().split(":")[1].split(".")[0]
-    // extract the data corresponding to this site
-    var residue_data = chart.data.filter(d => (+d.protein_site == site_name)
-                                         && (d.protein_chain === chain_name))
-    // there should not be more than one entry
+      // extract the data corresponding to this site
+    var residue_data = chart.condition_data.filter(d => (+d.protein_site ==
+        site_name) && (d.protein_chain === chain_name))
+      // there should not be more than one entry
     try {
-      if(residue_data.length > 1) throw "data parse wrong";
-    }catch(err) {
+      if (residue_data.length > 1) throw "data parse wrong";
+    } catch (err) {
       console.log(err)
     }
     // if there are no entries ues NaN data
-    if (residue_data.length == 0){
+    if (residue_data.length == 0) {
       residue_data = nan_data
-    }else{
+    } else {
       residue_data = residue_data[0]
     }
 
     // write to the tooltip
-    tooltip.innerHTML = `Atom: ${chain_name} ${site_name}
+    tooltip.innerHTML =
+      `Atom: ${chain_name} ${site_name}
        <hr/>
        site: ${residue_data.site}<br/>
        site label: ${residue_data.label_site}<br/>
@@ -154,7 +164,7 @@ stage.signals.hovered.add(function (pickingProxy) {
     tooltip.style.bottom = cp.y + 3 + "px";
     tooltip.style.left = cp.x + 3 + "px";
     tooltip.style.display = "block";
-  }else{
+  } else {
     tooltip.style.display = "none";
   }
 });
