2e96b668abe01057e9a2a6e56d80df64125b5209
skhilton <skhilton@uw.edu>
2020-01-17 14:56:55 -0800

    metakey turns brush into an erase

--- docs/_javascript/line_plot_zoom.js
+++ docs/_javascript/line_plot_zoom.js
@@ -456,6 +456,17 @@ function genomeLineChart() {
         lastBrushTypeClick = this.value;
       });
 
+      // add listener pressing a key on the keyboard
+      // if metaKey down, change brush to eraser
+      window.addEventListener("keydown", event => {
+        if (event.metaKey) {
+          d3.select("#brushmenu").property("value", "deselect")
+        }
+      });
+      window.addEventListener("keyup", event => {
+          d3.select("#brushmenu").property("value", "select")
+      });
+
       // Handler for dropdown value change
       dropdownChange = function() {
         current_condition = d3.select("#condition").property('value');
