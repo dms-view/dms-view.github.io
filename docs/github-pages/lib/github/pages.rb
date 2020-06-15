71bc0910e8f5214b96111461092a956dbccb18c7
skhilton <skhilton@uw.edu>
2020-06-15 12:36:50 -0700

    deleted docs subdirectory

--- docs/github-pages/lib/github/pages.rb
+++
@@ -1,8 +0,0 @@
-require "github/pages/version"
-
-module Github
-  module Pages
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- docs/github-pages/lib/github/pages/version.rb
+++
@@ -1,5 +0,0 @@
-module Github
-  module Pages
-    VERSION = "0.1.0"
-  end
-end
